a85581d1f3ac38921e423a4f490659e4721c5b6f
munchkinhalfling <munchkin@rutins.com>
2019-05-24 14:28:49 -0400

    Removing excess stuff

--- genconf/strutils.h
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-#include <stdlib.h>
-
-char *quote(char *unquoted) {
-  char *retval = malloc((strlen(unquoted)+2)*sizeof(char));
-  sprintf(retval, "\"%s\"", unquoted);
-  return retval;
-}
